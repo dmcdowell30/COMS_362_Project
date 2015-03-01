27ed038ad11113d5cdc920f83fc032d0c009e7ee
arlenb <arlenb@iastate.edu>
2015-03-01 15:56:33 -0600

    DatabaseSupport functions added.

--- src/iteration1/DatabaseSupport.java
+++ src/iteration1/DatabaseSupport.java
@@ -1,5 +1,78 @@
 package iteration1;
 
-public class DatabaseSupport {
+import java.util.ArrayList;
 
+
+
+public class DatabaseSupport {
+	
+	
+	private String query(String command){
+		String result = "";
+		
+		
+		
+		return result;
+	}
+	
+	
+	/////////////////////////////////////
+	////// Below are from Class Diagram
+	//////////////////////////
+	
+	
+	boolean putLibrarian(Librarian l){
+		boolean success = false;
+		
+		return success;
+	}
+	
+	boolean removeLibrarian(String username){
+		boolean success = false;
+		
+		return success;
+	}
+	
+	boolean putCustomer(Customer c){
+		boolean success = false;
+		
+		return success;
+	}
+	
+	Inventory RequestInventory(){
+		Inventory inventory = new Inventory();
+		
+		return inventory;
+	}
+	
+	Customer getCustomer(int id){
+		Customer customer;
+		
+		return null;
+	}
+	
+	ArrayList<Customer> getCustomerList(){
+		
+		ArrayList<Customer> customerList = new ArrayList<Customer>();
+		
+		return customerList;
+	}
+	
+	boolean removeCustomer(int id){
+		boolean success = false;
+		
+		return success;
+	}
+	
+	boolean putInventoryItem(Inventory i){
+		boolean success = false;
+		
+		return success;
+	}
+	
+	boolean removeInventoryItem(String code){
+		boolean success = false;
+		
+		return success;
+	}
 }
